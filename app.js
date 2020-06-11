96e15b2fd53f8d582175cf28c5fb6df25d884af4
mel <melissa.sargent@hotmail.co.uk>
2020-06-11 16:24:52 +0100

    it works it works it works

--- app.js
+++ app.js
@@ -1,73 +1,67 @@
-// let picsArray = ["cloud", "moon", "mountain", "rainbow", "tree", "stars", "cloud", "moon", "mountain", "rainbow", "tree", "stars"]
-// const cardImage = document.querySelectorAll(".board div");
-// let selection = [];
-// //set random images to board//
-// // for(let i = picsArray.length - 1; i > 0; i--){
-// //   const j = Math.floor(Math.random() * i)
-// //   const temp = picsArray[i]
-// //   picsArray[i] = picsArray[j]
-// //   picsArray[j] = temp
-// // }
-// // let image = new Image();
-// // image.src = './assets.moon.jpg'
-// // document.getElementById("1").appendChild('image');
-// cardImage.forEach(pic => {
-
-// })
-// // click to reveal cards//
-// cardImage.forEach(card => {
-//   card.addEventListener('click', () => {
-//     selection.push(EventTarget.attribute('src')); 
-//     console.log(selection); 
-//   });
-
-// });
-
-//////////////////
 let numArray = ["moon", "moon", "tree", "tree", "rainbow", "rainbow", "cloud", "cloud", "mountain", "mountain", "star", "star"];
-const card = document.querySelectorAll(".board div");
+const cards = document.querySelectorAll(".board div");
 let selection = [];
 let clicks = 0;
+let score = 0;
+let scoreHTML = document.querySelector(".score p")
+
 
 //shuffles numArray//
-for(let i = numArray.length - 1; i > 0; i--){
-    const j = Math.floor(Math.random() * i)
-    const temp = numArray[i]
-    numArray[i] = numArray[j]
-    numArray[j] = temp
-  }
+for (let i = numArray.length - 1; i > 0; i--) {
+  const randomPosition = Math.floor(Math.random() * i);
+  const shuffleItem = numArray[i];
+  numArray[i] = numArray[randomPosition];
+  numArray[randomPosition] = shuffleItem;
+}
+//functions for revealing images on cards//
 
-card.forEach((div) => {
+//assigns images to divs//
+cards.forEach((div) => {
   let shiftedNum = numArray.shift();
-  div.classList = shiftedNum;
-})
-
-card.forEach(card => {
-  card.addEventListener("click", () => {
-    selection.push(card.classList);
-    clicks++;
-        console.log(selection);
-        console.log(clicks)
-    if (clicks === 2) {
-      if (selection[0] === selection[1]) {
-        console.log("match")
-        card.removeEventListener("click");
-        selection = [];
-      } else {
-        return
-      }
-    }
-  })
+  div.classList = (shiftedNum);
+  div.classList.add('hidden');
 });
 
-    // checkformatch = (selection) => {
-    //   if (selection[0] === selection[1]) {
-    //     console.log("match")
-    //     card.removeEventListener("click");
-    //     selection = [];
-    //   } else {
-    //     return
-    //   }
-    // };
+//captures cards clicked//
+cards.forEach(card => {
+  card.onclick = () => {
+    card.classList.remove('hidden');
+    setTimeout(() => card.classList.add('hidden'), 2000);
+    selection.push(card);
+    clicks++;
+    if (clicks === 2) {
+      checkformatch();
+    }
+  }
+});
+//compares two cards clicked//
+checkformatch = () => {
+  const cardOne = selection[0];
+  const cardTwo = selection[1];
+
+  if (cardOne.classList.value === cardTwo.classList.value) {
+    console.log("Pair");
+    setTimeout(() => cardOne.classList.add('rehide'), 1000);
+    setTimeout(() => cardTwo.classList.add('rehide'), 1000);
+    cardOne.onclick = null;
+    cardTwo.onclick = null;
+    selection = [];
+    clicks = 0;
+    score ++;
+    scoreHTML.innerHTML = score;
+  } else {
+    selection = [];
+    clicks = 0;
+  }
+};
+
+gameOver = () => {
+  if (score = (numArray.length / 2)) {
+    location.reload();
+    score = 0;
+    clicks = 0;
+    };
+  };
+
 
 
